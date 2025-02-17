e3b7a2581e3518190132d6b480490b6d2371c4fa
Waleed Chayeb <waleedchayeb2@gmail.com>
2025-02-18 01:18:14 +0300

    Rubik Font

--- resources/views/layouts/install.blade.php
+++ resources/views/layouts/install.blade.php
@@ -13,9 +13,8 @@
     {{-- Favicon --}}
     <link rel="shortcut icon" href="{{  asset(config('app.site.favicon')) }}" type="image/x-icon">
     {{-- (Preloading) Fonts --}}
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Rubik:400,300" rel="preload" as="style"> 
+    
     {{-- Global CSS Assets --}}
     {!!
         Minify::stylesheet(
